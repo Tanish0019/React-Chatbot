ca9811356c2bd1c9fd5e02b0554495c4acc84c8f
Tanish Grover <tanigrover@gmail.com>
2018-12-10 02:27:29 +0530

    chalja bhai

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
   dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
   dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
   googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+  googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 };
